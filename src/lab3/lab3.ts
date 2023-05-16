661f79573959472894ba62865d4f14e42aa42db2
Dmitrii <ultra.las664@gmail.com>
2023-05-16 20:58:35 +0300

    Доработка к первым 3 частям

--- src/lab3/lab3.ts
+++ src/lab3/lab3.ts
@@ -1,8 +1,7 @@
 class Cart {
     public static _instance: Cart;
-    private constructor() {
-        
-    }
+
+    private constructor() {}
 
     public static getInstance(): Cart {
         return this._instance || (this._instance = new this());
