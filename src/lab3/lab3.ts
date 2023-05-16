a0990415512ef354c2bbd857aaf81b5d70306e85
Dmitrii <ultra.las664@gmail.com>
2023-05-16 21:28:49 +0300

    Выполнены все пункты кроме 5 и 9 в лабе 3

--- src/lab3/lab3.ts
+++ src/lab3/lab3.ts
@@ -1,10 +1,25 @@
+interface Item {
+    title: string;
+    price: string;
+    amount: number;
+}
+
 class Cart {
     public static _instance: Cart;
+    private items: any;
 
     private constructor() {}
 
     public static getInstance(): Cart {
         return this._instance || (this._instance = new this());
+    }
+
+    public addItem({item}: Item) {
+
+    }
+
+    public getItems() {
+        return this.items
     }
 
     
