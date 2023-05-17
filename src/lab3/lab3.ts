c7fc2d0fe4a92a1e4f7d1f69433daa8185a96f1b
Dmitrii <ultra.las664@gmail.com>
2023-05-17 22:37:09 +0300

    Все задания выполнены

--- src/lab3/lab3.ts
+++ src/lab3/lab3.ts
@@ -1,27 +1,47 @@
 interface Item {
-    title: string;
-    price: string;
-    amount: number;
+  title: string;
+  price: string;
+  amount: number;
 }
 
-class Cart {
-    public static _instance: Cart;
-    private items: any;
+export default class Cart {
+  public static _instance: Cart;
+  private items = [
+    {
+      title: "Macbook Air 13",
+      price: "80000",
+      amount: 1,
+    },
+    {
+      title: "Macbook Pro 16",
+      price: "180000",
+      amount: 1,
+    },
+  ];
 
-    private constructor() {}
+  private constructor() {}
 
-    public static getInstance(): Cart {
-        return this._instance || (this._instance = new this());
+  public static getInstance(): Cart {
+    return this._instance || (this._instance = new this());
+  }
+  public addItem(item: Item) {
+    {
+      this.items.map((item) => {
+        if (item.title in this.items) {
+          item.amount += 1;
+        } else {
+          this.items.push(item);
+        }
+      });
     }
+  }
 
-    public addItem(item: Item) {
+  public removeItem(item: Item) {
+    const index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
 
-    }
-
-    public getItems() {
-        return this.items
-    }
-
-    
-
+  public getItems() {
+    return this.items;
+  }
 }
